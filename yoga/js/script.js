470829f63527a1552ef52bb8cb8f33429a064a85
Asylkhan <asylkhan1703@mail.ru>
2020-08-27 15:03:25 +0500

    Был добавлен таймер отсчета

--- yoga/js/script.js
+++ yoga/js/script.js
@@ -1,9 +1,22 @@
 window.addEventListener('DOMContentLoaded', function () {
     'use strict';
+
+
+
     let infoHeader = document.querySelector('.info-header'),
         infoHeaderTab = document.querySelectorAll('.info-header-tab'),
         tabContent = document.querySelectorAll('.info-tabcontent');
 
+    // CSS styles:
+    // .content.info.hide {
+    //         display: none;
+    //     }
+    // .content.info.show {
+    //         display: -webkit - box;
+    //         display: -webkit - flex;
+    //         display: -ms - flexbox;
+    //         display: flex;
+    //     }
     function hideTabContent(a) {
         for (let i = a; i < tabContent.length; i++) {
             tabContent[i].classList.remove('show');
@@ -31,6 +44,47 @@ window.addEventListener('DOMContentLoaded', function () {
             }
         }
 
-});
+    });
+
+    //Timer
+    let deadline = '2020-08-29';
+
+    function getTimeRemaining(endtime) {
+        let t = Date.parse(endtime) - Date.parse(new Date()),
+            seconds = Math.floor((t / 1000) % 60),
+            minutes = Math.floor((t / 1000 / 60) % 60),
+            hours = Math.floor((t / (1000 * 60 * 60)) % 24);
+
+        return {
+            'total': t,
+            'hours': hours,
+            'minutes': minutes,
+            'seconds': seconds
+        };
+    }
+
+    function setClock(id, endtime) {
+        let timer = document.getElementById(id),
+            hours = timer.querySelector('.hours'),
+            minutes = timer.querySelector('.minutes'),
+            seconds = timer.querySelector('.seconds'),
+            timeInterval = setInterval(updateClock, 1000);
+        
+        function updateClock() {
+            let t = getTimeRemaining(endtime);
+            hours.textContent = t.hours;
+            minutes.textContent = t.minutes;
+            seconds.textContent = t.seconds;
+
+            if (t.total <= 0) {
+                clearInterval(timeInterval);
+            }
+        }
+    }
+
+    setClock('timer', deadline);
+
+
+    
 });
 
